51de5f5148e0e52235de6868c82ec1f63802b711
Diego <96896483+molot7@users.noreply.github.com>
2024-09-28 02:05:20 -0600

    Add files via upload

--- Dashboard.py
+++ Dashboard.py
@@ -1,10 +1,7 @@
-import pip
-pip.main(["install","plotly"])
-
 import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
-import numpy as np
+
 
  
 st.set_page_config(page_title="pagina 2",layout="wide")
