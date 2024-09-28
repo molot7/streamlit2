ba1dbb559a677d607538730d5d73a32c311691ef
Diego <96896483+molot7@users.noreply.github.com>
2024-09-28 02:37:05 -0600

    Add files via upload

---
+++ Datos.py
@@ -0,0 +1,47 @@
+import streamlit as st
+import pandas as pd
+
+
+#nobre pagina e icono
+st.set_page_config(page_title="Hola",page_icon="👋",layout="wide")
+
+st.markdown(
+    """
+    <style>
+    .block-container {
+        padding-top: 28px;  /* Ajusta el padding superior según sea necesario */
+        padding-left: 10px; /* Ajusta el padding izquierdo según sea necesario */
+        padding-right: 20px; /* Ajusta el padding derecho según sea necesario */
+        padding-bottom: 10px; /* Ajusta el padding inferior según sea necesario */
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+st.write("### Creado por Diego para perfil de OCC")
+
+#df = pd.read_csv('apartments.csv', delimiter=';',encoding='latin1')# el df es temporal luego se sustituira por el archivo subido
+df = pd.read_csv('Forbes2.csv')
+
+
+st.write("#### Exploracion de los datos")
+st.dataframe(df.head(200),hide_index=True)
+
+
+c1, c2,c3,c4 = st.columns(4)
+
+c1.container(border=True).metric('Cantidad de filas',df.shape[0] , delta_color="normal", label_visibility="visible")  
+c2.container(border=True).metric('Número de variables', df.shape[1], delta_color="normal", label_visibility="visible") 
+c3.container(border=True).metric('Número de variables categoricas', len(df.columns[df.dtypes=="O"]), delta_color="normal", label_visibility="visible")
+c4.container(border=True).metric('Número de variables numéricas', len(df.select_dtypes(include=['number']).columns), delta_color="normal", label_visibility="visible")
+
+nul=df.isnull().sum()
+nulos=pd.DataFrame({'datos nulos por columna':nul})
+
+st.dataframe(nulos,hide_index=False)
+
+
+
+
+
